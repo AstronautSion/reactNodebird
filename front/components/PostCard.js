0706ee9fe78a7596297f0d0bd38c75b0f4db7dca
AstronautSion <astronaut.sion@gmail.com>
2021-02-25 19:37:14 +0900

    add postCardForm

--- front/components/PostCard.js
+++ front/components/PostCard.js
@@ -1,40 +1,23 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import {Card, Icon, Button, Avatar, List, Form, Input, Comment} from 'antd';
+import React, { useCallback, useState } from 'react';
+import {Card, Icon, Button, Avatar, List, Comment} from 'antd';
 import PropTypes from 'prop-types';
-import { useSelector, useDispatch } from 'react-redux';
-import { ADD_COMMENT_REQUEST } from '../reducers/post';
+import { useSelector } from 'react-redux';
+import PostCardForm from '../components/PostCardForm';
 
 const PostCard = ({ post }) =>{
     const [ commentFormOpend, setCommentFormOpend] = useState(false);
-    const [ commentText, setCommentText] = useState('');
     const { me } = useSelector(state => state.user);
-    const { commentAdded, isAddingComment } = useSelector(state => state.post);
-    const dispatch = useDispatch();
     
-
     const onToggleComment = useCallback(() =>{
         if(!me){
             return alert('로그인이 필요합니다.');
         }
         setCommentFormOpend(prev => !prev);
     },[me && me.id])
-    const onSubmitComment = useCallback((e)=>{
-        e.preventDefault();
-        dispatch({
-            type: ADD_COMMENT_REQUEST,
-            data: { content: commentText, postId: post.id, userId: me.id },
-          });
-    },[commentText]);
-    const onChangeCommentText = useCallback((e)=>{
-        setCommentText(e.target.value);
-    },[]);
-    
-    useEffect(()=>{
-        setCommentText('');
-    },[commentAdded === true])
     
     return(
         <>  
+            
             {post.User && 
                 <Card
                     key={post.createdAt}
@@ -56,12 +39,7 @@ const PostCard = ({ post }) =>{
             }
                 {commentFormOpend && (
                     <>
-                        <Form onSubmit={onSubmitComment}>
-                            <Form.Item>
-                                <Input.TextArea rows={4} value={commentText} onChange={onChangeCommentText} />
-                            </Form.Item>
-                            <Button type="primary" htmlType="submit" loading={isAddingComment}>삐약</Button>
-                        </Form>
+                        <PostCardForm post={post} />
                         <List 
                             header={`${post.Comments ? post.Comments.length : 0} 댓글`}
                             itemLayout="horizontal"
